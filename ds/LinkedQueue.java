bc852e904213e8314a6908b5f05469043645d14f
Nawaf-Alsalem <nawafalsalem80@gmail.com>
2026-04-27 21:56:17 +0300

    Implemented LinkedQueue methods and added nested class Node: isEmpty enqueue dequeue percolateDown toList

--- ds/LinkedQueue.java
+++ ds/LinkedQueue.java
@@ -1,13 +1,70 @@
 package kfupm.clinic.ds;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /** Students implement. */
 public class LinkedQueue<T> {
-    public void enqueue(T item) { throw new UnsupportedOperationException("TODO: LinkedQueue.enqueue"); }
-    public T dequeue() { throw new UnsupportedOperationException("TODO: LinkedQueue.dequeue"); }
-    public boolean isEmpty() { throw new UnsupportedOperationException("TODO: LinkedQueue.isEmpty"); }
+    // we'll create our own implementation of Node DS
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> front;
+    private Node<T> rear;
+
+
+    public LinkedQueue() {
+        front = null;
+        rear = null;
+    }
+
+    public void enqueue(T item) {
+        Node<T> newNode = new Node<>(item);
+
+        if (isEmpty()) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+
+        T value = front.data;
+        front = front.next;
+
+        if (front == null) {
+            rear = null;
+        }
+
+        return value;
+    }
+    public boolean isEmpty() {
+        return front == null;
+    }
 
     /** Non-destructive view for printing. */
-    public List<T> toList() { throw new UnsupportedOperationException("TODO: LinkedQueue.toList"); }
+    public List<T> toList() {
+        List<T> list = new ArrayList<>();
+        Node<T> current = front;
+
+        while (current != null) {
+            list.add(current.data);
+            current = current.next;
+        }
+
+        return list;
+    }
 }
