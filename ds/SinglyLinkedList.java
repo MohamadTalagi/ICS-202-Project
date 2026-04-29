475950b3d9bd065f446cd086f01dc19247247f2a
Nawaf-Alsalem <nawafalsalem80@gmail.com>
2026-04-29 19:15:55 +0300

    Implemented SinglyLinkedList methods and added nested class Node: addLast toList

--- ds/SinglyLinkedList.java
+++ ds/SinglyLinkedList.java
@@ -1,9 +1,49 @@
 package kfupm.clinic.ds;
+import java.util.ArrayList;
 
 import java.util.List;
 
 /** Students implement. */
 public class SinglyLinkedList<T> {
-    public void addLast(T item) { throw new UnsupportedOperationException("TODO: SinglyLinkedList.addLast"); }
-    public List<T> toList() { throw new UnsupportedOperationException("TODO: SinglyLinkedList.toList"); }
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public void addLast(T item) {
+        if (item == null) throw new IllegalArgumentException("item is null");
+
+        Node<T> newNode = new Node<>(item);
+
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    public List<T> toList() {
+        List<T> list = new ArrayList<>();
+
+        Node<T> current = head;
+
+        while (current != null) {
+            list.add(current.data);
+            current = current.next;
+        }
+
+        return list;
+    }
 }
